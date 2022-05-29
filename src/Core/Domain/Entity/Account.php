5ff4085907027d1f426ddc5fee1232ddbf831e82
Kevin Farias <kevin.v.farias@gmail.com>
2022-05-29 13:38:26 -0300

    feat: add the folder structure

--- src/Core/Domain/Entity/Account.php
+++ src/Core/Domain/Entity/Account.php
@@ -7,12 +7,18 @@ use Core\Domain\Entity\Traits\MethodsMagicsTrait;
 class Account {
     use MethodsMagicsTrait;
 
+    private $id;
+    private $balance;
+    private $transactions = [];
+
     public function __construct(
-        private string $id,
-        private float $balance,
-        private \DateTime|string $createdAt = ''
+        string $id,
+        float $balance,
+        \DateTime|string $createdAt = ''
     ) {
-        $this->createdAt = $this->createdAt ? new \DateTime($this->createdAt) : new \DateTime();
+        $this->id = $id;
+        $this->balance = $balance;
+        $this->createdAt = $createdAt ? new \DateTime($createdAt) : new \DateTime();
         $this->validate();
     }
 
@@ -27,5 +33,9 @@ class Account {
         }
 
         return true;
+    }
+
+    public function balance() {
+        return (float)$this->balance;
     }
 }
