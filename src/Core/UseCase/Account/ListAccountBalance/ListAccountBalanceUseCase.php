2f0172741165667a7df43fb2afa234739f116138
Kevin Farias <kevin.v.farias@gmail.com>
2022-05-29 16:39:51 -0300

    feat(account): add list account balance use case

---
+++ src/Core/UseCase/Account/ListAccountBalance/ListAccountBalanceUseCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core\UseCase\Account\ListAccountBalance;
+
+use Core\Domain\Account\Repository\AccountRepositoryInterface;
+use Core\Domain\Shared\Errors\NotFoundError;
+use Core\UseCase\Account\ListAccountBalance\DTO\ListAccountBalanceInputDto;
+
+class ListAccountBalanceUseCase
+{
+    private $repository;
+
+    public function __construct(AccountRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(ListAccountBalanceInputDto $inputDto): ListAccountBalanceResponseDto
+    {
+        $account = $this->repository->findById($inputDto->id);
+
+        if (!$account) {
+            throw new NotFoundError();
+        }
+
+        return new ListAccountBalanceResponseDto($account->id(), $account->balance());
+    }
+}
