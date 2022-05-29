01074f6fd0834632f673d051b2e276b5d4b04c43
Kevin Farias <kevin.v.farias@gmail.com>
2022-05-29 16:21:20 -0300

    feat: add the account repository

---
+++ src/Core/Domain/Account/Repository/AccountRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\Domain\Account\Repository;
+
+use Core\Domain\Account\Account;
+
+interface AccountRepositoryInterface
+{
+    public function insert(Account $account): Account;
+    public function findById(string $id): Account;
+    public function update(Account $account): Account;
+    public function delete(Account $account): void;
+}
